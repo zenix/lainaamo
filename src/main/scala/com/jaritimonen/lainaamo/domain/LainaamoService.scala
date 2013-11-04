e557cee62a84f72861d498225a76d431acad439b
Jari Timonen <jari.timonen@zeniitti.net>
2013-11-04 22:26:59 +0200

    remove comment

--- src/main/scala/com/jaritimonen/lainaamo/domain/LainaamoService.scala
+++ src/main/scala/com/jaritimonen/lainaamo/domain/LainaamoService.scala
@@ -3,13 +3,7 @@ package com.jaritimonen.lainaamo.domain
 import com.jaritimonen.lainaamo.infrastructure.BookRepository
 import com.mongodb.casbah.commons.MongoDBObject
 import com.mongodb.casbah.Imports._
-/**
- * Created with IntelliJ IDEA.
- * User: saha
- * Date: 2.11.2013
- * Time: 18:31
- * To change this template use File | Settings | File Templates.
- */
+
 class LainaamoService {
   def findByName(name: String): List[Book] = {
     val toFind = "^" + name +".*"
